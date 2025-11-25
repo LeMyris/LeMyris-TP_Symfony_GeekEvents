68b4dc722924def64954d8563d2a423565a4f64b
Maxime NASH <mnash.cda@FORMATION.LOCAL>
2025-11-25 12:59:07 +0400

    extension de la classe utilisateur

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Activite;
 use App\Entity\Utilisateur;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
